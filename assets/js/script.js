80e4e2b398ab59bf84d0802b3f06c9ac91181b10
KKramerio <136101246+KKramerio@users.noreply.github.com>
2023-07-01 18:18:42 +0100

    updates for compliance

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,14 @@
 // script.js
 function changeImage(images) {
     var currentIndex = 0;
-    var image = document.getElementById('image');
-  
+    var image = document.getElementById("image");
     function updateImage() {
       image.src = images[currentIndex];
-  
       currentIndex++;
       if (currentIndex >= images.length) {
         currentIndex = 0;
       }
     }
-  
     updateImage();
-  
-    setInterval(updateImage, 2000); // Change image every 2 seconds (adjust as needed)
+    setInterval(updateImage, 3000); //Change image every 3 sec
   }
-  
